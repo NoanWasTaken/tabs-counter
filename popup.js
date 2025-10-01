6f9bfaf1490a26fc91509f8fc2e262aa92ea6fd4
NoanWasTaken <delatouchenoan@gmail.com>
2025-10-01 16:01:26 +0200

    <DEV> Added dark and light mode

--- popup.js
+++ popup.js
@@ -1,18 +1,18 @@
 function updatePopup() {
-    browser.tabs.query({}).then((tabs) => {
-        document.getElementById("count").textContent = tabs.length;
-    });
-    let oldTabs = 0;
-    const oneDay = 24 * 60 * 60 * 1000;
-    const now = Date.now();
-    browser.tabs.query({}).then(tabs => {
-        for (let tab of tabs) {
-            if (tab.lastAccessed && (now - tab.lastAccessed) > oneDay) {
-                oldTabs++;
-            }
-        }
-        document.getElementById("old-count").innerHTML = "(" + oldTabs + ")";
-    });
+  browser.tabs.query({}).then((tabs) => {
+    document.getElementById("count").textContent = tabs.length;
+  });
+  let oldTabs = 0;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const now = Date.now();
+  browser.tabs.query({}).then(tabs => {
+    for (let tab of tabs) {
+      if (tab.lastAccessed && (now - tab.lastAccessed) > oneDay) {
+        oldTabs++;
+      }
+    }
+    document.getElementById("old-count").innerHTML = "(" + oldTabs + ")";
+  });
 }
 
 document.getElementById("close-old").addEventListener("click", () => {
@@ -30,3 +30,31 @@ document.getElementById("close-old").addEventListener("click", () => {
 
 // Initialisation
 updatePopup();
+
+
+
+
+const body = document.body;
+const toggleButton = document.getElementById("theme-toggle");
+const icon = document.getElementById("theme-icon");
+
+browser.storage.local.get("theme").then(result => {
+  const theme = result.theme || "light";
+  applyTheme(theme);
+});
+
+toggleButton.addEventListener("click", () => {
+  const newTheme = body.classList.contains("dark") ? "light" : "dark";
+  applyTheme(newTheme);
+  browser.storage.local.set({ theme: newTheme });
+});
+
+function applyTheme(theme) {
+  if (theme === "dark") {
+    body.classList.add("dark");
+    icon.src = "images/moon-sat.svg";
+  } else {
+    body.classList.remove("dark");
+    icon.src = "images/sun-light.svg";
+  }
+}
